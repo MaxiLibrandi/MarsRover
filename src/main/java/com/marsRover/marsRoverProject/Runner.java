391bc1322bbf93ae379d11a7510260a1cf9c9ab4
Maximo Librandi <maximolibrandi@hotmail.com>
2021-05-29 12:08:28 -0300

    Parser added

--- src/main/java/com/marsRover/marsRoverProject/Runner.java
+++ src/main/java/com/marsRover/marsRoverProject/Runner.java
@@ -1,13 +1,30 @@
 package com.marsRover.marsRoverProject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import com.marsRover.marsRoverProject.parser.Parser;
 
 public class Runner {
 private final static Logger LOGGER = LogManager.getLogger(Runner.class);
 
 	public static void main(String[] args) {
-		LOGGER.info("Hello World");
+		String inputFilePath = "src/main/resources/input.txt";
+		Parser inputParser = new Parser(inputFilePath);
+		inputParser.readFile();
+		
+		BufferedReader bufferedReader = new BufferedReader(new StringReader(inputParser.getFileContent()));
+		String line = null;
+		try {
+			line = bufferedReader.readLine();
+			LOGGER.info("Plateau size is " + line);
+			while ((line = bufferedReader.readLine()) != null)
+				LOGGER.info(line);
+		} catch (IOException e) {
+			LOGGER.error(e);
+		}		
 	}
 
 }
--- src/main/java/com/marsRover/marsRoverProject/parser/Parser.java
+++ src/main/java/com/marsRover/marsRoverProject/parser/Parser.java
@@ -1,5 +1,48 @@
 package com.marsRover.marsRoverProject.parser;
 
-public class Parser {
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Parser {
+	private final static Logger LOGGER = LogManager.getLogger(Parser.class);
+	private String filename;
+	private String fileContent;
+	
+	public Parser(String filename) {
+		this.filename = filename;
+	}
+	
+	public void readFile() {
+		BufferedReader bufferedReader = null;
+		try {
+			bufferedReader = new BufferedReader(new FileReader(filename));
+		    StringBuilder stringBuilder = new StringBuilder();
+		    String line = bufferedReader.readLine();
+		    while (line != null) {
+		    	stringBuilder.append(line);
+		    	stringBuilder.append(System.lineSeparator());
+		        line = bufferedReader.readLine();
+		    }
+		    fileContent = stringBuilder.toString();
+		} catch (FileNotFoundException e) {
+			LOGGER.error(e);
+		} catch (IOException e) {
+			LOGGER.error(e);
+		} finally {
+			try {
+				bufferedReader.close();
+			} catch (IOException e) {
+				LOGGER.error(e);
+			}
+		}
+	}
+	
+	public String getFileContent() {
+		return fileContent;
+	}
 }
