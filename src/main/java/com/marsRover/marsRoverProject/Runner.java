de198b334f548350b787c851ce53e22e251febec
Maximo Librandi <maximolibrandi@hotmail.com>
2021-05-28 21:48:05 -0300

    Logger added

--- src/main/java/com/marsRover/marsRoverProject/Runner.java
+++ src/main/java/com/marsRover/marsRoverProject/Runner.java
@@ -1,10 +1,13 @@
 package com.marsRover.marsRoverProject;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Runner {
+private final static Logger LOGGER = LogManager.getLogger(Runner.class);
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+		LOGGER.info("Hello World");
 	}
 
 }
